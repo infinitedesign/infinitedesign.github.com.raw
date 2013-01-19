733c6cfcd23ade0ace4ddf0e7656e7ebbd4b0496
Thomas I. Sample <thomas@limitlessimaginationstudios>
2013-01-19 08:54:43 -0500

    added and integrated nanoc template engine

---
+++ app/templates/nanoc-blank-slate/lib/default.rb
@@ -0,0 +1,28 @@
+# All files in the 'lib' directory will be loaded
+# before nanoc starts compiling.
+
+
+
+
+# To use this helper, activate the Rendering helper
+# (see the helpers section for details),
+# which is done by adding this line of code to some file in
+# the lib directory (I recommend lib/helpers.rb):
+# USE EXAMPLE:
+# <%= render 'head' %>     --OR--
+# <%= render "head", :title => "Foo" %>
+
+# To access uncomment:
+# include Nanoc::Helpers::Rendering
+
+
+
+
+# *********************
+# Check this stuff out:
+#
+# filter :erb
+# filter :kramdown
+# layout 'default'
+# filter :rubypants
+# *********************
